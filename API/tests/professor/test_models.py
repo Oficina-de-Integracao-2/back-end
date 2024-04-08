0c154fccc1276ea8d6ea8fab1611c1e65bc46b2e
ViniciusOgawa <vini_ogawa@hotmail.com>
2024-04-08 17:57:04 -0300

    fix#testes-professor, #1

--- API/tests/professor/test_models.py
+++ API/tests/professor/test_models.py
@@ -1,6 +1,8 @@
 import pytest
 from django.core.exceptions import ValidationError
 from professor.models import Professor
+from django.db import IntegrityError
+from django.db import transaction
 
 
 @pytest.mark.django_db
@@ -13,20 +15,20 @@ def test_create_professor():
 
     professor = Professor.objects.create(**professor_data)
 
-    assert Professor.objects.count() == 1
-
-    assert professor.name == professor_data["name"]
-    assert professor.email == professor_data["email"]
-    assert professor.cpf == professor_data["cpf"]
+    assert Professor.objects.count() == 1, "Deveria haver exatamente um professor no banco de dados."
+    assert professor.name == professor_data["name"], "O nome do professor não corresponde ao esperado."
+    assert professor.email == professor_data["email"], "O email do professor não corresponde ao esperado."
+    assert professor.cpf == professor_data["cpf"], "O CPF do professor não corresponde ao esperado."
 
 
 @pytest.mark.django_db
 def test_cpf_unique():
     Professor.objects.create(name="Prof. Carlos", email="carlos@example.com", cpf="11122233344")
 
-    with pytest.raises(ValidationError):
-        professor = Professor(name="Prof. Carlos", email="carlos2@example.com", cpf="11122233344")
-        professor.full_clean()
+    with pytest.raises(IntegrityError):
+        with transaction.atomic():
+            professor = Professor(name="Prof. Carlos", email="carlos2@example.com", cpf="11122233344")
+            professor.save()
 
 
 @pytest.mark.django_db
