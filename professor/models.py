2c653acb31cd1c6fe3052b0da8230c36ed63bb10
ViniciusOgawa <vini_ogawa@hotmail.com>
2024-04-08 16:51:49 -0300

    feature#testes-model-professor, #1

--- professor/models.py
+++ professor/models.py
@@ -1,7 +1,13 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+
+
+def validate_cpf_length(value):
+    if len(value) != 11:
+        raise ValidationError("CPF deve ter 11 dígitos.")
 
 
 class Professor(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11, unique=True)
+    cpf = models.CharField(max_length=11, unique=True, validators=[validate_cpf_length])
