91be6233de46ddcfd1df643f199d39d9816d6b15
ViniciusOgawa <vini_ogawa@hotmail.com>
2024-04-10 18:49:02 -0300

    fix#model-professor, #1

--- professor/models.py
+++ professor/models.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -7,7 +8,11 @@ def validate_cpf_length(value):
         raise ValidationError("CPF deve ter 11 dígitos.")
 
 
-class Professor(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11, unique=True, validators=[validate_cpf_length])
+class Professor(AbstractUser):
+    cpf = models.CharField(
+        max_length=11,
+        unique=True,
+        validators=[validate_cpf_length],
+        help_text="Informe um CPF com exatamente 11 dígitos."
+    )
+    email = models.EmailField(unique=True)
