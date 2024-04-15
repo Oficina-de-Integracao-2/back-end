3272f9edacba4cf15064e7069ce44b15ed7c1528
ViniciusOgawa <vini_ogawa@hotmail.com>
2024-04-14 21:30:22 -0300

    fix#url-name, #1 #3

--- professor/urls.py
+++ professor/urls.py
@@ -3,7 +3,7 @@ from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("professor/", views.ProfessorView.as_view()),
-    path("login/", TokenObtainPairView.as_view()),
-    path("refresh/", TokenRefreshView.as_view())
+    path("professor/", views.ProfessorView.as_view(), name='professor-list'),
+    path("login/", TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("refresh/", TokenRefreshView.as_view(), name='token_refresh')
 ]
