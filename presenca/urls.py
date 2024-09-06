157d3db68f018ed87b444647fe1b2e3597266216
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-09-06 16:02:36 -0300

    feature: criando rota para geracao de certificado, #8

--- presenca/urls.py
+++ presenca/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PresencaListCreate, PresencaListByOficina, UnmarkPresence
+from .views import PresencaListCreate, PresencaListByOficina, UnmarkPresence, GenerateCertificate
 
 urlpatterns = [
     path('presenca/', PresencaListCreate.as_view(), name='presenca-list-create'),
     path('presenca/oficina/<int:oficina_id>/', PresencaListByOficina.as_view(), name='presenca-list-by-oficina'),
     path('presenca/unmark/<int:student_id>/<int:workshop_id>/', UnmarkPresence.as_view(), name='unmark-presenca'),
+    path('certificate/generate/<int:student_id>/<int:workshop_id>/', GenerateCertificate.as_view(), name='generate-certificate'),
 ]
--- presenca/views.py
+++ presenca/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from .models import Presenca
+from aluno.models import Aluno
+from oficina.models import Oficina
 from .serializers import PresencaSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,6 +9,13 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from aluno.serializers import AlunoSerializer
 from rest_framework import status
+from django.http import HttpResponse
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import getSampleStyleSheet
+from textwrap import wrap
+from reportlab.lib.enums import TA_JUSTIFY
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Frame, Spacer
 
 
 class PresencaListCreate(generics.ListCreateAPIView):
@@ -39,3 +48,56 @@ class UnmarkPresence(APIView):
             return Response({"message": "Presence successfully unmarked."}, status=status.HTTP_200_OK)
         except Presenca.DoesNotExist:
             return Response({"error": "Presence not found."}, status=status.HTTP_404_NOT_FOUND)
+
+class GenerateCertificate(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, student_id, workshop_id):
+        try:
+            presenca = Presenca.objects.get(aluno_id=student_id, oficina_id=workshop_id, presente=True)
+            aluno = presenca.aluno
+            oficina = presenca.oficina
+
+            response = HttpResponse(content_type='application/pdf')
+            response['Content-Disposition'] = f'attachment; filename="certificado_{aluno.nome}_{oficina.title}.pdf"'
+
+            p = SimpleDocTemplate(response, pagesize=A4)
+
+            styles = getSampleStyleSheet()
+            style = styles["BodyText"]
+            style.alignment = TA_JUSTIFY  # Justify text
+
+            text = f"""
+            Certificamos que {aluno.nome}, participou da oficina '{oficina.title}' realizada em {oficina.city_of_realization}, no dia {oficina.date_of_realization.strftime('%d/%m/%Y')}, com carga horária de {oficina.workload} horas.
+            """
+
+            paragraph = Paragraph(text, style)
+
+            elements = []
+
+            header_text = "ELLP - Ensino Lúdico: Lógica e Programação"
+            header_paragraph = Paragraph(f"<b>{header_text}</b>", styles['Title'])
+            elements.append(header_paragraph)
+
+            title_text = "Certificado de Participação"
+            title_paragraph = Paragraph(f"<b>{title_text}</b>", styles['Heading2'])
+            elements.append(title_paragraph)
+
+            elements.append(paragraph)
+
+            signature_text = """
+            <br/><br/><br/>
+            ______________________________________<br/>
+            Assinatura do Responsável<br/>
+            ELLP - Ensino Lúdico: Lógica e Programação
+            """
+            signature_paragraph = Paragraph(signature_text, style)
+            elements.append(signature_paragraph)
+
+            p.build(elements)
+
+            return response
+
+        except Presenca.DoesNotExist:
+            return HttpResponse("Certificado não disponível. Presença não registrada.", status=404)
