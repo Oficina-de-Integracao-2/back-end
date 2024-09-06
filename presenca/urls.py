543d21094324329e656f4c35e72052dcbacf3d0d
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-09-06 15:27:40 -0300

    feature#adicionando rota de remover presenca de um aluno

--- presenca/urls.py
+++ presenca/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import PresencaListCreate, PresencaListByOficina
+from .views import PresencaListCreate, PresencaListByOficina, UnmarkPresence
 
 urlpatterns = [
     path('presenca/', PresencaListCreate.as_view(), name='presenca-list-create'),
     path('presenca/oficina/<int:oficina_id>/', PresencaListByOficina.as_view(), name='presenca-list-by-oficina'),
-
+    path('presenca/unmark/<int:student_id>/<int:workshop_id>/', UnmarkPresence.as_view(), name='unmark-presenca'),
 ]
--- presenca/views.py
+++ presenca/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from aluno.serializers import AlunoSerializer
+from rest_framework import status
 
 
 class PresencaListCreate(generics.ListCreateAPIView):
@@ -25,3 +26,16 @@ class PresencaListByOficina(APIView):
         alunos_presentes = [presenca.aluno for presenca in presencas]
         serializer = AlunoSerializer(alunos_presentes, many=True)
         return Response(serializer.data)
+
+class UnmarkPresence(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, student_id, workshop_id):
+        try:
+            presence = Presenca.objects.get(aluno_id=student_id, oficina_id=workshop_id)
+            presence.presente = False
+            presence.save()
+            return Response({"message": "Presence successfully unmarked."}, status=status.HTTP_200_OK)
+        except Presenca.DoesNotExist:
+            return Response({"error": "Presence not found."}, status=status.HTTP_404_NOT_FOUND)
